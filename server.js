d99cbf584c960d59a1247b8ce200e4c658942737
William Macfarlane <william.macfarlane@tulip.io>
2017-05-12 20:56:25 -0400

    Try new http listen args

--- server.js
+++ server.js
@@ -57,9 +57,10 @@ app.get('/noodle', function(req, res) {
 			\nLove, W');
 });
 
+var server_ip  = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
 var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
 
-http.listen(server_port, function() { console.log("Listening on  localhost:8080")});
+http.listen(server_port, server_ip, 511, function() { console.log("Listening on  localhost:8080")});
 
 //var server = app.listen(server_port, server_ip_address, function(){
 //  console.log("Listening on " + server_ip_address + ", server_port " + server_port)
